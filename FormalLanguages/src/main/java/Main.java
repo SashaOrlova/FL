5d3327989bcbc68dfa587066477df00ea90859ac
SashaOrlova <sasha.orlova.1998n@mail.ru>
2018-04-22 23:24:09 +0300

    fix

--- FormalLanguages/src/main/java/Main.java
+++ FormalLanguages/src/main/java/Main.java
@@ -1,16 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            Scanner in = new Scanner(System.in);
-            String st = in.nextLine();
-            try {
-                Tree t = new Parser(new Lexer(st).getResult()).parseExpr();
-                t.print(0);
-            } catch (Parser.ParserException | Lexer.LexerException parserException) {
-                System.out.println(parserException.getMessage());
+            File f = new File(args[0]);
+        try {
+            FileReader fr = new FileReader(f);
+            Scanner in = new Scanner(fr);
+            while (in.hasNextLine()) {
+                String st = in.nextLine();
+                try {
+                    Tree t = new Parser(new Lexer(st).getResult()).parseExpr();
+                    t.print(0);
+                } catch (Parser.ParserException | Lexer.LexerException parserException) {
+                    System.out.println(parserException.getMessage());
+                }
             }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error in input file");
         }
     }
 }
